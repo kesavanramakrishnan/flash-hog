c4f3c5f909fd02c74055cc2f1118f24083bcb0d9
Marcel Rød <marcelroed@gmail.com>
2026-02-27 09:37:15 -0800

    Most of the underlying implementations for the jax attention implementation

---
+++ fhog/jax/_attention_impl.py
@@ -0,0 +1,64 @@
+"""
+Implementations for the functions used in fhog.jax.attention.
+"""
+
+from functools import partial
+
+import jax
+from jax._src.cudnn.fused_attention_stablehlo import MaskType
+from jax._src.cudnn.fused_attention_stablehlo import dot_product_attention as cuda_dot_product_attention
+from jax.tree_util import Partial
+
+from fhog.pallas_bwdbwd import TuningConfig, flash_bwdbwd0
+
+
+def dot_product_attention_fwd(query, key, value, mask_type: MaskType, scale: float):
+    """
+    Forward pass, no saving.
+    Only needs to return the output.
+    """
+    return cuda_dot_product_attention(query, key, value, mask_type=mask_type, scale=scale)
+
+
+def dot_product_attention_fwd_rule(query, key, value, mask_type: MaskType, scale: float):
+    """
+    Forward pass, saving stats, Q, K, V and O.
+    """
+    out, vjp_fun = jax.vjp(partial(cuda_dot_product_attention, mask_type=mask_type, scale=scale), query, key, value)
+    residual = vjp_fun
+    return out, residual
+
+
+def dot_product_attention_bwd_rule(mask_type: MaskType, scale: float, res, g):
+    """
+    Backward pass, no saving
+    """
+    vjp_fun = res
+    dout = vjp_fun(g)
+    return dout
+
+
+def dot_product_attention_bwd_rule_fwd_rule(mask_type: MaskType, scale: float, res, g):
+    """
+    Backward pass, saving for higher order backward
+    """
+    vjp_fun = res
+    dO = g
+
+    query, key, value = vjp_fun.args_res
+    *_, stats, out = vjp_fun.opaque_residuals
+    dQ, dK, dV = vjp_fun(dO)
+    residual = (query, key, value, stats, out, dO)
+
+    return (dQ, dK, dV), residual
+
+
+def dot_product_attention_bwd_rule_bwd_rule(mask_type: MaskType, scale: float, res, g):
+    """
+    Backward pass through the backward pass
+    """
+    query, key, value, stats, out, dO = res
+    ddQ, ddK, ddV = g
+
+    dQ2, dK2, dV2, ddO = flash_bwdbwd0(query, key, value, out, dO, ddQ, ddK, ddV, stats, scale, config=TuningConfig(tile_q=128, tile_k=128, max_concurrent_steps=4))
+    return dQ2, dK2, dV2, ddO
